403273eb93d0777e8f7f70473ca8feeb589128e9
Marcell Tóth <tmarcellt.2000@gmail.com>
2024-07-16 18:57:59 +0200

    Suppress keybind

--- src/main.py
+++ src/main.py
@@ -14,4 +14,4 @@ def play_sound() -> None:
 if __name__ == '__main__':
     keyboard.add_hotkey('ctrl+shift+alt+q', play_sound, suppress=True)
     print("Press Ctrl+Shift+Alt+Q to play the sound.")
-    keyboard.wait('ctrl+shift+alt+w')
+    keyboard.wait('ctrl+shift+alt+w', suppress=True)
